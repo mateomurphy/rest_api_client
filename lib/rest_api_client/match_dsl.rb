58563b6799c7eba546f733a8522ef315a090832d
Mateo Murphy <mateo.murphy@gmail.com>
2013-02-23 20:16:35 -0500

    Started resource oriented DSL

--- lib/rest_api_client/match_dsl.rb
+++ lib/rest_api_client/match_dsl.rb
@@ -3,8 +3,11 @@ require 'uri_template'
 module RestApiClient 
   module MatchDsl
     class Matcher
+      include Inflector
+
       def initialize(connection)
         @connection = connection
+        @scope = []
       end
 
       def match(data = {})
@@ -23,6 +26,37 @@ module RestApiClient
         end
       end
 
+      def resource(name, options = {})
+        name = name.to_s
+        match scoped_name(name) => scoped_path(name)
+
+        with_scope name, :singular do
+          yield 
+        end if block_given?
+      end
+
+      def resources(name, options = {})
+        name = name.to_s
+        match scoped_name(name) => scoped_path(name), scoped_name(singularize(name)) => scoped_path("#{name}/:id")
+
+        with_scope name, :plural do
+          yield 
+        end if block_given?
+      end
+
+      def scoped_name(*name)
+        (@scope.map { |d| singularize(d.first) } + name).join("_")
+      end
+
+      def scoped_path(*path)
+        (@scope.map { |d| d.last == :plural ? "#{d.first}/:#{d.first}" : d.first} + path).join("/") 
+      end
+
+      def with_scope(name, type)
+        @scope << [name, type]
+        yield 
+        @scope.pop
+      end
     end
 
     class Resource
@@ -32,7 +66,6 @@ module RestApiClient
         @connection = connection
         @url = url
       end
-
     end
   end
 end
--- spec/match_dsl_spec.rb
+++ spec/match_dsl_spec.rb
@@ -1,20 +1,45 @@
 require 'spec_helper'
 
-
 describe 'Match DSL Spec' do
-  subject :client do
-    RestApiClient.match('http://api.compant.com') do |c|
-      c.match 'accounts' => 'accounts'
-      c.match 'account' => 'accounts/:id'
-      c.match 'account_projects' => 'accounts/:account/projects'
-      c.match 'account_project' => 'accounts/:account/projects/:id'
+  context 'using #match' do
+    subject :client do
+      RestApiClient.match('http://api.compant.com') do |c|
+        c.match :accounts => 'accounts'
+        c.match :account => 'accounts/:id'
+        c.match :account_projects => 'accounts/:account/projects'
+        c.match :account_project => 'accounts/:account/projects/:id'
+      end
+    end
+
+    describe 'generated methods' do
+      specify { client.accounts.url.should eq('accounts') }
+      specify { client.account(1).url.should eq('accounts/1') }
+      specify { client.account_projects(1).url.should eq('accounts/1/projects') }
+      specify { client.account_project(1, 2).url.should eq('accounts/1/projects/2') }
     end
   end
 
-  describe 'generated methods' do
-    specify { client.accounts.url.should eq('accounts') }
-    specify { client.account(1).url.should eq('accounts/1') }
-    specify { client.account_projects(1).url.should eq('accounts/1/projects') }
-    specify { client.account_project(1, 2).url.should eq('accounts/1/projects/2') }
+  context 'using #resources' do
+    subject :client do
+      RestApiClient.match('http://api.compant.com') do |c|
+        c.resources :accounts do
+          c.resources :projects
+          c.resource :settings
+        end
+        c.resource :user do
+          c.resources :projects
+        end
+      end
+    end
+
+    describe 'generated methods' do
+      specify { client.accounts.url.should eq('accounts') }
+      specify { client.account(1).url.should eq('accounts/1') }
+      specify { client.account_projects(1).url.should eq('accounts/1/projects') }
+      specify { client.account_project(1, 2).url.should eq('accounts/1/projects/2') }
+      specify { client.account_settings(1).url.should eq('accounts/1/settings') }
+      specify { client.user.url.should eq('user') }
+      specify { client.user_projects.url.should eq('user/projects') }
+    end
   end
 end
